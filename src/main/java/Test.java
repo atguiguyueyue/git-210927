6a26a9b0a50a145dc6ea9102e0f40bc3fa1f4c5c
SH0927 <SH0927@atguigu.com>
2022-02-12 15:52:57 +0800

    冲突

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("aaaaaaaaaaaa");
         System.out.println("hot-fix");
         System.out.println("冲突测试");
     }

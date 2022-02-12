1dafdc69597113c78ed786581e8c1b494d9ae9cf
SH0927 <SH0927@atguigu.com>
2022-02-12 15:52:05 +0800

    冲

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("hot-fix");
+        System.out.println("冲突测试");
     }
 }

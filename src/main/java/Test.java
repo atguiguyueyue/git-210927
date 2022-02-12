9b7a5160184290d25d22fde0d8de3b9dfddcc70a
SH0927 <SH0927@atguigu.com>
2022-02-12 15:53:11 +0800

    冲突111

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("bbbbbbbbbbb");
         System.out.println("abc");
         System.out.println("master");
         System.out.println("hot-fix");
